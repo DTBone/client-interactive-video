00b06e74b7798c1b4e144a60d10cf0d524f8afdf
thanhbinh1411 <thanhbinhdang1411@gmail.com>
2024-09-17 17:13:47 +0700

    Enroll to the course

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,17 @@
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 // Create a theme instance.
 const theme = extendTheme({
     componentSize: {
         appBarHeight: '48px',
         boardBarHeight: '58px',
     },
+    fontFamily: 'Roboto, sans-serif',
     colorSchemes: {
         light: {
             palette: {
