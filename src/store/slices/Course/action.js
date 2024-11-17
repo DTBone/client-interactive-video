bd81f0fdea6d79b754ae04d13e8efd7863a067c8
thanhbinh1411 <thanhbinhdang1411@gmail.com>
2024-11-18 03:03:16 +0700

    edit merge code

--- src/store/slices/Course/action.js
+++ src/store/slices/Course/action.js
@@ -7,8 +7,8 @@ export const getAllCourse = createAsyncThunk(
     async (filter, { rejectWithValue }) => {
         try {
 
-            const { data } = await axiosInstance.get("/learns");
-            return data;
+            // const { data } = await axiosInstance.get("/learns");
+            // return data;
 
             const { data } = await api.get("/learns", {
                 params: {
