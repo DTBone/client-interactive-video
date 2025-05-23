5707dd312eb1ff7e20502a0311537310ed6ce486
thanhbinh1411 <thanhbinhdang1411@gmail.com>
2025-05-24 01:21:31 +0700

    fix

--- src/modules/Lesson/Components/InteractiveDialogInFullscreen .jsx
+++ src/modules/Lesson/Components/InteractiveDialogInFullscreen .jsx
@@ -100,11 +100,7 @@ const InteractiveDialogInFullscreen = ({
           style={{
             position: "absolute",
             top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: "flex",
-            alignItems: "center",
+          
             justifyContent: "center",
             background: "rgba(0,0,0,0.5)",
             zIndex: 1,
